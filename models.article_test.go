f05652aa5c53317a0ed455d069a489b23ed7f1ba
Muhamad Risman <real.risman@gmail.com>
2018-11-19 01:53:19 +0700

    Add model article

---
+++ models.article_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+// Test the function that fetches all articles
+func TestGetAllArticles(t *testing.T) {
+	alist := getAllArticles()
+
+	if len(alist) != len(articleList) {
+		t.Fail()
+	}
+
+	// Check that each number is identical
+	for i, v := range alist {
+		if v.Content != articleList[i].Content ||
+			v.ID != articleList[i].ID ||
+			v.Title != articleList[i].Title {
+
+			t.Fail()
+			break
+		}
+	}
+}
